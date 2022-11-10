f18fe65a157389487d10078fb84f1d1684f82482
Rob Hess <hessro@oregonstate.edu>
2022-11-10 18:31:07 -0500

    Add code from 11/10 lecture.

--- files.js
+++ files.js
@@ -1,13 +1,13 @@
 var fs = require("fs")
 
-// fs.readFile("./hello.js", "utf8", function (err, data) {
-//     if (!err) {
-//         console.log(data)
-//     }
-// })
+fs.readFile("./hello.js", "utf8", function (err, data) {
+    if (!err) {
+        console.log(data)
+    }
+})
 
-// console.log("== This comes after fs.readFile()")
-// console.log("== This also comes after fs.readFile()")
+console.log("== This comes after fs.readFile()")
+console.log("== This also comes after fs.readFile()")
 
-var data = fs.readFileSync("./hello.js", "utf8")
-console.log(data)
+// var data = fs.readFileSync("./hello.js", "utf8")
+// console.log(data)
